4a3eb367e2b457192a658871da06bea4945212eb
elitan <johan@eliasson.me>
2020-07-17 17:59:19 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "components/App";
+import Login from "components/Login";
+import PrivateRoute from "components/PrivateRoute";
+import { auth } from "utils/nhost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { NhostAuthProvider, NhostApolloProvider } from "react-nhost";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NhostAuthProvider auth={auth}>
+      <NhostApolloProvider
+        auth={auth}
+        gql_endpoint="https://hasura-zgp96xwz.nhost.app/v1/graphql"
+      >
+        <Router>
+          <Switch>
+            <PrivateRoute exact path="/">
+              <App />
+            </PrivateRoute>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </NhostApolloProvider>
+    </NhostAuthProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
